419896a5609bbdc73fdf5c324be5c85860dac8fd
liuhongjun <liuhongjun@foryou56.com>
2021-04-14 19:34:27 +0800

    t

--- src/main/java/com/vcola/leetcode/puzzle/RemoveDuplicatesFromSortedArray_26.java
+++ src/main/java/com/vcola/leetcode/puzzle/RemoveDuplicatesFromSortedArray_26.java
@@ -8,7 +8,20 @@ import java.util.Arrays;
  * @author Very Cola
  * @date 2018年5月25日 上午9:28:03
  */
-public class RemoveDuplicatesFromSortedArray {
+public class RemoveDuplicatesFromSortedArray_26 {
+
+  public int removeDuplicates(int[] nums) {
+    if (nums == null || nums.length == 0) {
+      return 0;
+    }
+    int i = 0;
+    for (int j = 1; j < nums.length; j++) {
+      if (nums[i] != nums[j]) {
+        nums[++i] = nums[j];
+      }
+    }
+    return i + 1;
+  }
 
   /**
    * 返回移除后数组的新长度
@@ -16,11 +29,10 @@ public class RemoveDuplicatesFromSortedArray {
    * @param nums
    * @return
    */
-  public int removeDuplicates(int[] nums) {
+  public int removeDuplicatesV1(int[] nums) {
     if (nums == null || nums.length == 0) {
       return 0;
     }
-
     int length = 1;
     for (int i = 1; i < nums.length; i++) {
       if (nums[length - 1] != nums[i]) {
@@ -30,15 +42,12 @@ public class RemoveDuplicatesFromSortedArray {
         length++;
       }
     }
-
     return length;
   }
 
   public static void main(String[] args) {
     int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
-
-    RemoveDuplicatesFromSortedArray removeDuplicates = new RemoveDuplicatesFromSortedArray();
-
+    RemoveDuplicatesFromSortedArray_26 removeDuplicates = new RemoveDuplicatesFromSortedArray_26();
     int newLenght = removeDuplicates.removeDuplicates(nums);
     System.out.println(newLenght);
     System.out.println(Arrays.toString(nums));
